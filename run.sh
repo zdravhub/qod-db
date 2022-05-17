8f876790d0b492af008214bd516b8d89b1f5d852
Deepak C Shetty <dpkshetty@gmail.com>
2022-05-17 22:38:35 +0530

    Creatign QOD tables via run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-echo "********* Starting Deepak's script ************"
+echo "********* Creating QOD tables ************"
 mysql $mysql_flags < /tmp/1_createdb.sql
+mysql $mysql_flags < /tmp/2_authors.sql
+mysql $mysql_flags < /tmp/3_genres.sql
+mysql $mysql_flags < /tmp/4_quotes_sm.sql
